ee550e97749304a6d4437e54ce2e197809644860
ktrd <chelovek17361999@gmail.com>
2024-12-01 06:36:38 +0000

    refactoring

--- 2024/1/s.js
+++ 2024/1/s.js
@@ -1,7 +1,6 @@
-const input = require("fs").readFileSync("./bigboy.txt", "utf-8").split("\n");
-input.pop();
+const input = require("fs").readFileSync("./input", "utf-8").trim().split("\n");
 
-const {left, right} = input
+const { left, right } = input
   .map((l) => l.split("   ").map((n) => parseInt(n)))
   .reduce(
     (acc, [left, right]) => {
@@ -17,18 +16,9 @@ right.sort((a, b) => a - b);
 const answer1 = left.reduce((acc, a, i) => (acc += Math.abs(a - right[i])), 0);
 console.log(answer1);
 
-const map = right.reduce((acc, r) => {
-  if (acc.has(r)) {
-    acc.set(r, acc.get(r) + 1);
-  } else {
-    acc.set(r, 1);
-  }
-  return acc;
-}, new Map());
-const answer2 = left.reduce((acc, l) => {
-  if (map.has(l)) {
-    acc += l * map.get(l);
-  }
-  return acc;
-}, 0);
+const map = right.reduce(
+  (acc, r) => acc.set(r, (acc.get(r) || 0) + 1),
+  new Map(),
+);
+const answer2 = left.reduce((acc, l) => (acc += l * (map.get(l) || 0)), 0);
 console.log(answer2);
