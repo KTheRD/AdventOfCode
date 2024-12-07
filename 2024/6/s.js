2b5c5f4c4d18ce76e3003d4d2cebaa09eb3e3a33
ktrd <chelovek17361999@gmail.com>
2024-12-07 06:33:08 +0000

    Some fixes to day 6

--- 2024/6/s.js
+++ 2024/6/s.js
@@ -3,27 +3,13 @@ const input = require("fs")
   .trim()
   .split("\n")
   .map((l) => l.split(""));
-// const input = `....#.....
-// .........#
-// ..........
-// ..#.......
-// .......#..
-// ..........
-// .#..^.....
-// ........#.
-// #.........
-// ......#...`
-//   .split("\n")
-//   .map((l) => l.split(""));
 
-const guardPosOriginal = [];
-
-console.log(input.length, input[0].length);
+const initialGuardPosition = [];
 
 for (let y = 0; y < input.length; y++) {
   for (let x = 0; x < input[y].length; x++) {
     if (input[y][x] === "^") {
-      guardPosOriginal.push(y, x);
+      initialGuardPosition.push(y, x);
     }
   }
 }
@@ -35,50 +21,56 @@ for (let y = 0; y < input.length; y++) {
       continue;
     }
 
-    const inputCopy = input.map((l) => l.map((c) => c));
+    const inputCopy = input.map((l) => l.slice());
     inputCopy[y][x] = "#";
 
-    const guardPos = [...guardPosOriginal];
+    const guardPos = [...initialGuardPosition];
     let guardFace = "^";
     let loop = false;
 
     while (inputCopy[guardPos[0]]?.[guardPos[1]] !== undefined) {
       if (
         inputCopy[guardPos[0]]?.[guardPos[1]] === guardFace &&
-        guardPos[0] !== guardPosOriginal[0] &&
-        guardPos[1] !== guardPosOriginal[1] 
+        guardPos[0] !== initialGuardPosition[0] &&
+        guardPos[1] !== initialGuardPosition[1]
       ) {
         loop = true;
         break;
       }
-      if (guardFace === "^") {
-        if (inputCopy[guardPos[0] - 1]?.[guardPos[1]] !== "#") {
-          inputCopy[guardPos[0]][guardPos[1]] = "^";
-          guardPos[0] -= 1;
-        } else {
-          guardFace = ">";
-        }
-      } else if (guardFace === ">") {
-        if (inputCopy[guardPos[0]]?.[guardPos[1] + 1] !== "#") {
-          inputCopy[guardPos[0]][guardPos[1]] = ">";
-          guardPos[1] += 1;
-        } else {
-          guardFace = "v";
-        }
-      } else if (guardFace === "v") {
-        if (inputCopy[guardPos[0] + 1]?.[guardPos[1]] !== "#") {
-          inputCopy[guardPos[0]][guardPos[1]] = "v";
-          guardPos[0] += 1;
-        } else {
-          guardFace = "<";
-        }
-      } else if (guardFace === "<") {
-        if (inputCopy[guardPos[0]]?.[guardPos[1] - 1] !== "#") {
-          inputCopy[guardPos[0]][guardPos[1]] = "<";
-          guardPos[1] -= 1;
-        } else {
-          guardFace = "^";
-        }
+
+      switch (guardFace) {
+        case "^":
+          if (inputCopy[guardPos[0] - 1]?.[guardPos[1]] !== "#") {
+            inputCopy[guardPos[0]][guardPos[1]] = "^";
+            guardPos[0] -= 1;
+          } else {
+            guardFace = ">";
+          }
+          break;
+        case ">":
+          if (inputCopy[guardPos[0]]?.[guardPos[1] + 1] !== "#") {
+            inputCopy[guardPos[0]][guardPos[1]] = ">";
+            guardPos[1] += 1;
+          } else {
+            guardFace = "v";
+          }
+          break;
+        case "v":
+          if (inputCopy[guardPos[0] + 1]?.[guardPos[1]] !== "#") {
+            inputCopy[guardPos[0]][guardPos[1]] = "v";
+            guardPos[0] += 1;
+          } else {
+            guardFace = "<";
+          }
+          break;
+        case "<":
+          if (inputCopy[guardPos[0]]?.[guardPos[1] - 1] !== "#") {
+            inputCopy[guardPos[0]][guardPos[1]] = "<";
+            guardPos[1] -= 1;
+          } else {
+            guardFace = "^";
+          }
+          break;
       }
     }
 
