9fdc036560b803dcaeb8b45903759387724ca7de
ktrd <chelovek17361999@gmail.com>
2024-12-16 03:37:03 +0000

    Some changes to script Also tried to do rust a bit

--- aoc.sh
+++ aoc.sh
@@ -35,8 +35,24 @@ else
 fi
 
 cat <<EOL >"s.mjs"
-import {readFileSync} from 'fs'
-const input = readFileSync('./input', 'utf-8').trim().split('\\n')
+import { readFileSync } from "fs";
+
+const input = readFileSync("./input", "utf-8").trim();
+
+function parseInput(input) {
+  return input.split("\n").map((line) => line.split(""));
+}
+
+function part1(input) {
+    
+}
+
+function part2(input) {
+    
+}
+
+console.log(part1(parseInput(input)));
+console.log(part2(parseInput(input)));
 EOL
 
 nv s.mjs
